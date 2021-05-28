f818c9733091b691ad8baae16eb185f41b622ad8
Red <cyontjr@naver.com>
2021-05-28 15:33:26 +0900

    update 파일 다운2

--- bbs/file_download.php
+++ bbs/file_download.php
@@ -1,7 +1,7 @@
 <?php
     $file_name = $_GET['upload_file'];
+    preg_replace('/../','WWW',$file_name);
     $file_path = "./upload/". $file_name;
-    preg_replace('/../','WWW',$file_path)
     $file_size = filesize($file_path);
     header("Pragma: public");
     header("expires: 0");
